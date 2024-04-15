985ab023e26f5629de53fa0587dd667e3a00ced3
MrBob79 <magnus.martensson79@gmail.com>
2024-04-15 23:22:19 +0200

    Add project files.

---
+++ KlassDiagram/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace KlassDiagram
+{
+    public class Bankkonto
+    {
+        private string Owner;
+        private double Saldo;
+        private int AccountNumber;
+        private string Stock;
+
+
+        public Bankkonto(String Owner, double Saldo, int AccountNumber)
+        {
+            this.Owner = Owner;
+            this.Saldo = Saldo;
+            this.AccountNumber = 0;
+            this.Stock = " ";
+
+        }
+        // Metod för att sätta in pengar
+        public void CashIn(double Saldo)
+        {
+
+        }
+        
+    }
+}
