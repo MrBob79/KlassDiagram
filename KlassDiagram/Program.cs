347bf76030699d1401024a738f61cd6d5aae8b3d
MrBob79 <165480250+MrBob79@users.noreply.github.com>
2024-04-16 17:04:43 +0200

    Update Program.cs

--- KlassDiagram/Program.cs
+++ KlassDiagram/Program.cs
@@ -1,70 +1,72 @@
 using System;
 
+public class Bankkonto
+{
+    // Attribut
+    private string Owner;
+    private double Saldo;
+    private int AccountNumber;
+    private string Stock;
+    private double Savings;
 
-    public class Bankkonto
+    // Konstruktor
+    public Bankkonto(string owner, double saldo, int accountNumber, double Saves)
     {
-        // Attribut
-
-        public string Owner;
-        private double Saldo;
-        private int AccountNumber;
-        private string Stock;
-
-        //Konstruktor
-
-        public Bankkonto(String Owner, double Saldo)
-        {
-            Owner = Owner;
-            Saldo = Saldo;
-            AccountNumber = 0;
-            Stock = " ";
-            Console.WriteLine("-----------------------------\n");
-
-        }
-        // Metod för att sätta in pengar
-        public void CashIn(double Cash)
-        {
-            Saldo += Cash;
-            Console.WriteLine($"{Cash}Kr har satts in på kontot!");
-            Console.WriteLine("-----------------------------\n");
-        }
-
-        // Metod för att ta ut pengar
-        public void CashOut(double Cash)
-        {
-            if (Cash <= Saldo)
-            {
-                Saldo -= Cash;
-                Console.WriteLine($"{Cash}Kr har tagits ut från kontot");
-                Console.WriteLine("-----------------------------\n");
-            }   
-            else
-            {
-                Console.WriteLine("Otillräckligt saldo för att göra uttag!!");
-                Console.WriteLine("-----------------------------\n");
-            }
-        }
-            // Metod för att sätta in månadssparande och i en aktie
-        public void MontlySavingsAndStock(double Savings, string Stock)
-        {
-
-            Console.WriteLine($"Månadssparande {Savings}Kr");
-            Console.WriteLine($"Vald Aktie: {Stock}");
-            Console.WriteLine("-----------------------------\n");
+        this.Owner = owner;
+        this.Saldo = saldo;
+        this.AccountNumber = accountNumber;
+        this.Stock = " ";
+        this.Savings = Saves;
+        Console.WriteLine("-----------------------------\n");
     }
 
-            // Metod för att skriva ut Bankinfo
-        public void PrintInfo()
+    // Metod för att sätta in pengar
+    public void CashIn(double cash)
+    {
+        this.Saldo += cash;
+        Console.WriteLine($"{cash}Kr har satts in på kontot!");
+        Console.WriteLine("-----------------------------\n");
+    }
+
+    // Metod för att ta ut pengar
+    public void CashOut(double cash)
+    {
+        if (cash <= this.Saldo)
         {
-            
-            Console.WriteLine($"\nKontos Ägare: {Owner}");
-            Console.WriteLine($"Saldo: {Saldo}");
-            Console.WriteLine($"KontoNrummer: {AccountNumber}");
-            Console.WriteLine($"Aktie: {Stock}");
+            this.Saldo -= cash;
+            Console.WriteLine($"{cash}Kr har tagits ut från kontot");
+            Console.WriteLine("-----------------------------\n");
         }
+        else
+        {
+            Console.WriteLine("Otillräckligt saldo för att göra uttag!!");
+            Console.WriteLine("-----------------------------\n");
+        }
+    }
 
-
-
-
+    // Metod för att sätta in månadssparande 
+    public void MontlySavings(double Saves)
+    {
+        this.Savings = Saves;
+        Console.WriteLine($"Månadssparande {Savings}Kr");
+        Console.WriteLine("-----------------------------\n");
 
     }
+    public void StockSaves(string stock)
+    { 
+        this.Stock = stock;
+        Console.WriteLine($"Vald Aktie: {stock}");
+        Console.WriteLine("-----------------------------\n");
+    }
+
+    // Metod för att skriva ut Bankinfo
+    public void PrintInfo()
+    {
+        Console.WriteLine($"\nKontos Ägare: {this.Owner}");
+        Console.WriteLine($"Saldo: {this.Saldo}");
+        Console.WriteLine($"KontoNrummer: {this.AccountNumber}");
+        Console.WriteLine($"Månadssparande: {this.Savings}");
+        Console.WriteLine($"Aktie: {this.Stock}");
+        
+    }
+}
